e6954fffe39b42910f2469996e9d85a172163257
psharma49 <47172192+psharma49@users.noreply.github.com>
2021-06-23 12:53:28 +0530

    Checking build failure

--- src/main/java/com/infosys/product1/Application.java
+++ src/main/java/com/infosys/product1/Application.java
@@ -10,6 +10,7 @@ public class Application {
 		System.out.println("Hello Maven");
 		logger.debug("hello Maven");
 		logger.info("this is a info message");
+		logger
 		logger.error("this is a error");
 		logger.trace("trace message");
 		logger.warn("this is a warn message");
